36f17b5529e0e4fa67eb1dbbbda45c4cbcf56718
Emmanuel Ihebom <eihebom1@gmail.com>
2021-03-27 15:04:29 -0600

    Code to display date and time

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let a = 5
-let b = 4
-let c = a*b
-console.log(c)
+let x = new Date()
+let h = x.getHours()%12||12
+let m = x.getMinutes()
+let s = x.getSeconds()
+console.log("Date, 24 hour time & time zone is: "+ x + ";"+ " 12 hour time is: "+ h +":" + m +":" +s)
+
 
 
 
